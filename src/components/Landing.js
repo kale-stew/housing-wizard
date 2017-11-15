8b15eb1b787d4486d49f892578f28266ba9ddc7a
Kylie Stewart <kylie.stewart@me.com>
2017-11-14 22:22:45 -0700

    Landing styled w/ Routes

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,26 +2,36 @@
 // login or register
 
 import React, { Component } from 'react';
+import './styles/Landing.css';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 export default class Landing extends Component {
     render() {
+        const { inputHeader } = styles;
         return(
-            <div className="header_container">
-                <div className="header_child">
-                    <div className="header_left_container">
-                        <img src="icon.png" alt="logo"/>
-                        <span className="houser-bold">Houser</span>
-                        <span className="dashboard">Dashboard</span>
+            <div className="landing-container">
+                <div className="landing-content">
+                <img src="Houser.png" alt="welcome to Houser"/>
+                    <div className="input-container">
+                        <h4 style={inputHeader}>Username</h4>
+                        <input type="text"/>
+                        <h4 style={inputHeader}>Password</h4>
+                        <input type="text"/>
                     </div>
-                    <div className="header_right_container">
-                        <Link to="/welcome">
-                            <button className="register-btn">Register</button>
-                        </Link>
+                    <div className="button-container">
+                        <Link to="/welcome" className="login-btn">Login</Link>
+                        <Link to="/welcome" className="register-btn">Register</Link>
                     </div>
                 </div>
             </div>
         )
+    }
+}
+
+const styles = {
+    inputHeader: {
+        fontSize: '20px',
+        marginBottom: '6px'
     }
 }
